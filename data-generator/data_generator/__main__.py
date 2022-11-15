ca5b4b0aab0e6c66524363793981f7fd7b923201
Nicola Massarenti <nicola.m@smartcow.ai>
2022-11-15 16:17:18 +0100

    Bugfix docker-compose

--- data-generator/data_generator/__main__.py
+++ data-generator/data_generator/__main__.py
@@ -2,6 +2,7 @@ import time
 import typer
 import base64
 import requests
+import os
 
 from data_generator import logger, Sender
 
@@ -11,30 +12,49 @@ app = typer.Typer(name="data-generator")
 
 @app.command()
 def from_url(
-    url: str = typer.Argument(..., help="The url from which to download the data", envvar="URL", show_envvar=True),
-    sleep: int = typer.Argument(..., help="The delay between two data generations", envvar="SLEEP", show_envvar=True),
-    endpoint: str = typer.Argument(
-        ..., help="The recipient of the generated data", envvar="ENDPOINT", show_envvar=True
+    url: str = typer.Option(
+        ...,
+        help="The url from which to download the data",
+        envvar="URL",
+        show_envvar=True,
+    ),
+    sleep: int = typer.Option(
+        ...,
+        help="The delay between two data generations",
+        envvar="SLEEP",
+        show_envvar=True,
+    ),
+    endpoint: str = typer.Option(
+        ...,
+        help="The recipient of the generated data",
+        envvar="ENDPOINT",
+        show_envvar=True,
     ),
 ):
     """
-    Retrieves an image from `url` and sends it to `endpoint` every `sleep` milliseconds
+    Generates data from a URL and sends it to the endpoint using Kafka
+    :param url: the url from which to download the data
+    :param sleep: the delay between two data generations
+    :param endpoint: the recipient of the generated data
+    :return: None
     """
     logger.info(
         "from-url - 'url': '{url}, 'sleep': {sleep}, 'endpoint': {endpoint}".format(
             url=url, sleep=sleep, endpoint=endpoint
         )
     )
-    # Downloading image from url and converting it to base64
-    image = base64.b64encode(requests.get(url).content)
 
-    logger.info("Sending image to url '{endpoint}'".format(endpoint=endpoint))
-    # Sending image
-    Sender.send(image=image, endpoint=endpoint)
+    while True:
+        # Downloading image from url and converting it to base64
+        image = base64.b64encode(requests.get(url).content)
 
-    logger.info("Sleeping {x}ms".format(x=sleep))
-    # Sleeping `sleep` milliseconds
-    time.sleep(sleep)
+        logger.info("Sending image to url '{endpoint}'".format(endpoint=endpoint))
+        # Sending image to endpoint
+        Sender.send(image=image, endpoint=endpoint, topic=os.getenv("TOPIC"))
+
+        logger.info("Sleeping {x}ms".format(x=sleep))
+        # Sleeping `sleep` milliseconds
+        time.sleep(sleep / 1000)
 
 
 @app.command()
@@ -43,6 +63,7 @@ def from_scraping(name: str):
     Performs scarping from `url` using the query `query` and sends the data to `endpoint` every `sleep` milliseconds
     """
     pass
+
 
 if __name__ == "__main__":
     app()
